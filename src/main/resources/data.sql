0d9718e2784168c29eda897f7d022c6382cf75ce
pathfind1103 <pathfind1103@gmail.com>
2025-07-08 12:14:17 +0300

    feat: initialize database schema and data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,24 @@
+-- Инициализация ролей
+INSERT INTO roles (role_id, name) VALUES
+(1, 'ADMIN'),
+(2, 'REGIONAL_ADMIN'),
+(3, 'AGENT');
+
+-- Инициализация глобального администратора
+INSERT INTO administrators (admin_uuid, role_id, region_id, name, surname, email, phone, password, telegram_tag, prefer_channel, referal, created_at, updated_at, is_blocked)
+VALUES (
+    gen_random_uuid(),
+    1, -- ADMIN
+    NULL, -- Нет региона для глобального администратора
+    'Admin',
+    'Global',
+    'admin.global@example.com',
+    '+49123456789',
+    'password', -- Какой-то пароль
+    '@globaladmin',
+    'email',
+    NULL, -- Глобальному администратору не нужна реферальная ссылка
+    CURRENT_TIMESTAMP,
+    CURRENT_TIMESTAMP,
+    false
+);
