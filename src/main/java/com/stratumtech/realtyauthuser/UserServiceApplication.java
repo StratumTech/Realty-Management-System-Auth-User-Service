c708c0bb3e59d13fbd1b6f37ede7c5aecc129e73
pathfind1103 <pathfind1103@gmail.com>
2025-07-08 11:17:16 +0300

    refactor: rename package and update app name

--- src/main/java/com/stratumtech/realtyauthuser/UserServiceApplication.java
+++ src/main/java/com/stratumtech/realtyauthuser/UserServiceApplication.java
@@ -1,4 +1,4 @@
-package com.stratumtech.realtyapi;
+package com.stratumtech.realtyauthuser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/stratumtech/realtyauthuser/UserServiceApplicationTests.java
+++ src/test/java/com/stratumtech/realtyauthuser/UserServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.stratumtech.realtyapi;
+package com.stratumtech.realtyauthuser;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
