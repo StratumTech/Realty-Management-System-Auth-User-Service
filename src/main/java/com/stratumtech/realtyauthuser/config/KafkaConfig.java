ac71e81e1cedbf31bb3e2562f9499091873122fe
pathfind1103 <pathfind1103@gmail.com>
2025-07-15 11:08:14 +0300

    feat(kafka): add Kafka consumer and config for agent/admin creation

---
+++ src/main/java/com/stratumtech/realtyauthuser/config/KafkaConfig.java
@@ -0,0 +1,13 @@
+package com.stratumtech.realtyauthuser.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.support.converter.JsonMessageConverter;
+
+@Configuration
+public class KafkaConfig {
+    @Bean
+    public JsonMessageConverter jsonMessageConverter() {
+        return new JsonMessageConverter();
+    }
+} 
