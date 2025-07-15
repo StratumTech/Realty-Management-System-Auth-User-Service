818de7ebcac6394503435de4a6e7ea5afce648c9
pathfind1103 <pathfind1103@gmail.com>
2025-07-15 13:55:00 +0300

    feat(agent): add AgentCreateDTO and AgentUpdateDTO

---
+++ src/main/java/com/stratumtech/realtyauthuser/dto/AgentCreateDTO.java
@@ -0,0 +1,21 @@
+package com.stratumtech.realtyauthuser.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AgentCreateDTO {
+    private Integer roleId;
+    private String name;
+    private String patronymic;
+    private String surname;
+    private String email;
+    private String phone;
+    private String password;
+    private String telegramTag;
+    private String preferChannel;
+    private String imageUrl;
+}
