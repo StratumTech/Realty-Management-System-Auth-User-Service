6265d1202acad8469f88547aba06cb92235055a5
pathfind1103 <pathfind1103@gmail.com>
2025-07-16 20:59:19 +0300

    feat(admin): add AdministratorDTO and mapper

--- src/main/java/com/stratumtech/realtyauthuser/dto/mapper/AdministratorMapper.java
+++ src/main/java/com/stratumtech/realtyauthuser/dto/mapper/AdministratorMapper.java
@@ -1,4 +1,15 @@
 package com.stratumtech.realtyauthuser.dto.mapper;
 
+import com.stratumtech.realtyauthuser.entity.Administrator;
+import com.stratumtech.realtyauthuser.dto.AdministratorDTO;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import java.util.List;
+
+@Mapper(componentModel = "spring")
 public interface AdministratorMapper {
+    @Mapping(target = "role", source = "role.name")
+    @Mapping(target = "regionId", source = "region.id")
+    AdministratorDTO toDto(Administrator admin);
+    List<AdministratorDTO> toDtoList(List<Administrator> admins);
 }
