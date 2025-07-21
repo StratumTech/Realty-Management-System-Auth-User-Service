6a0acee5b2da000e28dcd8622418a5ec8d7a26e7
Pukhov Kirill <57557978+pukhovkirill@users.noreply.github.com>
2025-07-21 17:56:01 +0300

    fix: change id type in role repository

--- src/main/java/com/stratumtech/realtyauthuser/repository/RoleRepository.java
+++ src/main/java/com/stratumtech/realtyauthuser/repository/RoleRepository.java
@@ -5,6 +5,6 @@ import java.util.Optional;
 import com.stratumtech.realtyauthuser.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RoleRepository extends JpaRepository<Role, Integer> {
+public interface RoleRepository extends JpaRepository<Role, Long> {
     Optional<Role> findByName(String name);
 } 
