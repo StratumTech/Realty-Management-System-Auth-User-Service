91b1916fa670a408f91c733124653f4e942e7fd8
Pukhov Kirill <57557978+pukhovkirill@users.noreply.github.com>
2025-07-21 17:56:20 +0300

    fix: change id type in region repository

--- src/main/java/com/stratumtech/realtyauthuser/repository/RegionRepository.java
+++ src/main/java/com/stratumtech/realtyauthuser/repository/RegionRepository.java
@@ -3,5 +3,5 @@ package com.stratumtech.realtyauthuser.repository;
 import com.stratumtech.realtyauthuser.entity.Region;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RegionRepository extends JpaRepository<Region, Integer> {
+public interface RegionRepository extends JpaRepository<Region, Long> {
 } 
